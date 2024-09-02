2540aff8c4164e3f76c90bab369aeb9a6a635db7
Logan Choi <lsjcwma@gmail.com>
2024-09-01 23:20:57 -0500

    Tabs set up for bridge edit later to fit deposit and withdraw

--- web/src/app/bridge/page.tsx
+++ web/src/app/bridge/page.tsx
@@ -5,6 +5,22 @@ import { ConnectButton } from "thirdweb/react";
 import thirdwebIcon from "@public/thirdweb.svg";
 import { client } from "../client";
 import { Button } from "@/components/ui/button";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+  import { Input } from "@/components/ui/input"
+  import { Label } from "@/components/ui/label"
+  import {
+    Tabs,
+    TabsContent,
+    TabsList,
+    TabsTrigger,
+  } from "@/components/ui/tabs"
 
 import localFont from "next/font/local";
 const jersey = localFont({src: "../Jersey_10/Jersey10-Regular.ttf"});
@@ -15,10 +31,58 @@ export default function Home() {
       <Header />
       <div className="flex flex-col justify-center items-center w-screen h-screen bg-[url('/img/map_0.png')]">
         <div className={jersey.className}>
-
-        <h1 className="text-8xl text-amber-200">
-            Bridge
-          </h1> 
+            <Tabs defaultValue="account" className="w-[400px]">
+                <TabsList className="grid w-full grid-cols-2">
+                    <TabsTrigger value="account">Account</TabsTrigger>
+                    <TabsTrigger value="password">Password</TabsTrigger>
+                </TabsList>
+                <TabsContent value="account">
+                    <Card>
+                    <CardHeader>
+                        <CardTitle>Account</CardTitle>
+                        <CardDescription>
+                        Make changes to your account here. Click save when you're done.
+                        </CardDescription>
+                    </CardHeader>
+                    <CardContent className="space-y-2">
+                        <div className="space-y-1">
+                        <Label htmlFor="name">Name</Label>
+                        <Input id="name" defaultValue="Pedro Duarte" />
+                        </div>
+                        <div className="space-y-1">
+                        <Label htmlFor="username">Username</Label>
+                        <Input id="username" defaultValue="@peduarte" />
+                        </div>
+                    </CardContent>
+                    <CardFooter>
+                        <Button>Save changes</Button>
+                    </CardFooter>
+                    </Card>
+                </TabsContent>
+                <TabsContent value="password">
+                    <Card>
+                    <CardHeader>
+                        <CardTitle>Password</CardTitle>
+                        <CardDescription>
+                        Change your password here. After saving, you'll be logged out.
+                        </CardDescription>
+                    </CardHeader>
+                    <CardContent className="space-y-2">
+                        <div className="space-y-1">
+                        <Label htmlFor="current">Current password</Label>
+                        <Input id="current" type="password" />
+                        </div>
+                        <div className="space-y-1">
+                        <Label htmlFor="new">New password</Label>
+                        <Input id="new" type="password" />
+                        </div>
+                    </CardContent>
+                    <CardFooter>
+                        <Button>Save password</Button>
+                    </CardFooter>
+                    </Card>
+                </TabsContent>
+                </Tabs>
 
           
         </div>
